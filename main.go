e64953237647f85c522db17f2acc80b4b5a41053
maixhashi <maixhashi@gmail.com>
2025-02-15 23:53:18 +0900

    feature/validatorの実装

--- main.go
+++ main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
-	"go-react-app/controller"
-	"go-react-app/db"
-	"go-react-app/repository"
-	"go-react-app/router"
-	"go-react-app/usecase"
+	"go-rest-api/controller"
+	"go-rest-api/db"
+	"go-rest-api/repository"
+	"go-rest-api/router"
+	"go-rest-api/usecase"
+	"go-rest-api/validator"
 )
 
 func main() {
 	db := db.NewDB()
+	userValidator := validator.NewUserValidator()
+	taskValidator := validator.NewTaskValidator()
 	userRepository := repository.NewUserRepository(db)
 	taskRepository := repository.NewTaskRepository(db)
-	userUsecase := usecase.NewUserUsecase(userRepository)
-	taskUsecase := usecase.NewTaskUsecase(taskRepository)
+	userUsecase := usecase.NewUserUsecase(userRepository, userValidator)
+	taskUsecase := usecase.NewTaskUsecase(taskRepository, taskValidator)
 	userController := controller.NewUserController(userUsecase)
 	taskController := controller.NewTaskController(taskUsecase)
 	e := router.NewRouter(userController, taskController)
