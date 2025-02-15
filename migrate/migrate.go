5c124b84ad1e2f025e60e7e5925b7d089cdd6312
maixhashi <maixhashi@gmail.com>
2025-02-15 10:33:21 +0900

    feature/データベースの設定

---
+++ migrate/migrate.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"go-react-app/db"
+	"go-react-app/model"
+)
+
+func main() {
+	dbConn := db.NewDB()
+	defer fmt.Println("Successfully Migrated")
+	defer db.CloseDB(dbConn)
+	dbConn.AutoMigrate(&model.User{}, &model.Task{})
+}
