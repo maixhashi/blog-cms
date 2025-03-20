c9f3ce8b79b87175edfe2a53f823f635345bbfe6
maixhashi <maixhashi@gmail.com>
2025-03-20 22:47:32 +0900

    refactor/swaggerによるbackendリファクタリング（LayoutComponentモデル）

--- backend/controller/layout_component_controller.go
+++ backend/controller/layout_component_controller.go
@@ -30,6 +30,15 @@ func NewLayoutComponentController(lcu usecase.ILayoutComponentUsecase) ILayoutCo
 	return &layoutComponentController{lcu}
 }
 
+// GetAllLayoutComponents ユーザーのすべてのレイアウトコンポーネントを取得
+// @Summary ユーザーのレイアウトコンポーネント一覧を取得
+// @Description ログインユーザーのすべてのレイアウトコンポーネントを取得する
+// @Tags layout-components
+// @Accept json
+// @Produce json
+// @Success 200 {array} model.LayoutComponentResponse
+// @Failure 500 {object} map[string]string
+// @Router /layout-components [get]
 func (lcc *layoutComponentController) GetAllLayoutComponents(c echo.Context) error {
 	userId := getUserIdFromToken(c)
 	
@@ -40,6 +49,17 @@ func (lcc *layoutComponentController) GetAllLayoutComponents(c echo.Context) err
 	return c.JSON(http.StatusOK, componentsRes)
 }
 
+// GetLayoutComponentById 指定されたIDのレイアウトコンポーネントを取得
+// @Summary 特定のレイアウトコンポーネントを取得
+// @Description 指定されたIDのレイアウトコンポーネントを取得する
+// @Tags layout-components
+// @Accept json
+// @Produce json
+// @Param componentId path int true "コンポーネントID"
+// @Success 200 {object} model.LayoutComponentResponse
+// @Failure 400 {object} map[string]string
+// @Failure 500 {object} map[string]string
+// @Router /layout-components/{componentId} [get]
 func (lcc *layoutComponentController) GetLayoutComponentById(c echo.Context) error {
 	userId := getUserIdFromToken(c)
 	
@@ -56,6 +76,17 @@ func (lcc *layoutComponentController) GetLayoutComponentById(c echo.Context) err
 	return c.JSON(http.StatusOK, componentRes)
 }
 
+// CreateLayoutComponent 新しいレイアウトコンポーネントを作成
+// @Summary 新しいレイアウトコンポーネントを作成
+// @Description ユーザーの新しいレイアウトコンポーネントを作成する
+// @Tags layout-components
+// @Accept json
+// @Produce json
+// @Param component body model.LayoutComponentRequest true "コンポーネント情報"
+// @Success 201 {object} model.LayoutComponentResponse
+// @Failure 400 {object} map[string]string
+// @Failure 500 {object} map[string]string
+// @Router /layout-components [post]
 func (lcc *layoutComponentController) CreateLayoutComponent(c echo.Context) error {
 	userId := getUserIdFromToken(c)
 	
@@ -72,6 +103,18 @@ func (lcc *layoutComponentController) CreateLayoutComponent(c echo.Context) erro
 	return c.JSON(http.StatusCreated, componentRes)
 }
 
+// UpdateLayoutComponent 既存のレイアウトコンポーネントを更新
+// @Summary レイアウトコンポーネントを更新
+// @Description 指定されたIDのレイアウトコンポーネントを更新する
+// @Tags layout-components
+// @Accept json
+// @Produce json
+// @Param componentId path int true "コンポーネントID"
+// @Param component body model.LayoutComponentRequest true "更新するコンポーネント情報"
+// @Success 200 {object} model.LayoutComponentResponse
+// @Failure 400 {object} map[string]string
+// @Failure 500 {object} map[string]string
+// @Router /layout-components/{componentId} [put]
 func (lcc *layoutComponentController) UpdateLayoutComponent(c echo.Context) error {
 	userId := getUserIdFromToken(c)
 	
@@ -94,6 +137,17 @@ func (lcc *layoutComponentController) UpdateLayoutComponent(c echo.Context) erro
 	return c.JSON(http.StatusOK, componentRes)
 }
 
+// DeleteLayoutComponent レイアウトコンポーネントを削除
+// @Summary レイアウトコンポーネントを削除
+// @Description 指定されたIDのレイアウトコンポーネントを削除する
+// @Tags layout-components
+// @Accept json
+// @Produce json
+// @Param componentId path int true "コンポーネントID"
+// @Success 204 "No Content"
+// @Failure 400 {object} map[string]string
+// @Failure 500 {object} map[string]string
+// @Router /layout-components/{componentId} [delete]
 func (lcc *layoutComponentController) DeleteLayoutComponent(c echo.Context) error {
 	userId := getUserIdFromToken(c)
 	
@@ -110,6 +164,19 @@ func (lcc *layoutComponentController) DeleteLayoutComponent(c echo.Context) erro
 	return c.NoContent(http.StatusNoContent)
 }
 
+// AssignToLayout レイアウトコンポーネントをレイアウトに割り当て
+// @Summary コンポーネントをレイアウトに割り当て
+// @Description 指定されたコンポーネントを特定のレイアウトに割り当てる
+// @Tags layout-components
+// @Accept json
+// @Produce json
+// @Param componentId path int true "コンポーネントID"
+// @Param layoutId path int true "レイアウトID"
+// @Param position body model.AssignLayoutRequest true "割り当て情報"
+// @Success 200 "OK"
+// @Failure 400 {object} map[string]string
+// @Failure 500 {object} map[string]string
+// @Router /layout-components/{componentId}/assign/{layoutId} [post]
 func (lcc *layoutComponentController) AssignToLayout(c echo.Context) error {
 	userId := getUserIdFromToken(c)
 	
@@ -131,6 +198,17 @@ func (lcc *layoutComponentController) AssignToLayout(c echo.Context) error {
 	return c.NoContent(http.StatusOK)
 }
 
+// RemoveFromLayout レイアウトコンポーネントをレイアウトから削除
+// @Summary コンポーネントをレイアウトから削除
+// @Description 指定されたコンポーネントをレイアウトから削除する
+// @Tags layout-components
+// @Accept json
+// @Produce json
+// @Param componentId path int true "コンポーネントID"
+// @Success 200 "OK"
+// @Failure 400 {object} map[string]string
+// @Failure 500 {object} map[string]string
+// @Router /layout-components/{componentId}/assign [delete]
 func (lcc *layoutComponentController) RemoveFromLayout(c echo.Context) error {
 	userId := getUserIdFromToken(c)
 	
@@ -147,6 +225,18 @@ func (lcc *layoutComponentController) RemoveFromLayout(c echo.Context) error {
 	return c.NoContent(http.StatusOK)
 }
 
+// UpdatePosition レイアウトコンポーネントの位置を更新
+// @Summary コンポーネントの位置を更新
+// @Description 指定されたコンポーネントの位置情報を更新する
+// @Tags layout-components
+// @Accept json
+// @Produce json
+// @Param componentId path int true "コンポーネントID"
+// @Param position body model.PositionRequest true "位置情報"
+// @Success 200 "OK"
+// @Failure 400 {object} map[string]string
+// @Failure 500 {object} map[string]string
+// @Router /layout-components/{componentId}/position [put]
 func (lcc *layoutComponentController) UpdatePosition(c echo.Context) error {
 	userId := getUserIdFromToken(c)
 	
